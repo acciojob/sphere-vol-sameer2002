21b415511375dace8fbcb9b38574c877fba8ea4a
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-25 14:31:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function volume_sphere() {
 	 var radius = document.getElementById('radius').value;
             radius = Math.abs(radius);
             var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+            volume = volume.toFixed(4);
             document.getElementById('volume').value = volume;
             return false;
   
